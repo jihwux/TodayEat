669cdcdfd6d11f60aff569798467ec936c6d1827
Hwan <jhxxx7@gmail.com>
2019-12-15 23:18:13 +0900

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
- 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
