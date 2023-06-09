be03204b9d001a0067840e7005cc10594da6c88b
JiHwan <hg_07@naver.com>
2023-06-09 17:02:45 +0900

    delete: session key

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const path = require('path');
-const session = require('express-session');
-const flash = require('connect-flash');
-const passport = require('passport');
-const helmet = require('helmet');
-const hpp = require('hpp');
-const RedisStore = require('connect-redis')(session);
-require('dotenv').config();
-var favicon = require('serve-favicon');
-
-
-const pageRouter = require('./routes/page');
-const authRouter = require('./routes/auth');
-const foodsaveRouter = require('./routes/foodsave');
-const { sequelize } = require('./models');
-const passportConfig = require('./passport');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const helmet = require("helmet");
+const hpp = require("hpp");
+require("dotenv").config();
+var favicon = require("serve-favicon");
+const pageRouter = require("./routes/page");
+const authRouter = require("./routes/auth");
+const foodsaveRouter = require("./routes/foodsave");
+const { sequelize } = require("./models");
+const passportConfig = require("./passport");
 
 const app = express();
 sequelize.sync();
 passportConfig(passport);
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.set('port', process.env.PORT || 8001);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 8001);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(morgan('combined'));
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
   app.use(helmet());
   app.use(hpp());
 } else {
-  app.use(morgan('dev'));
+  app.use(morgan("dev"));
 }
-app.use(favicon(path.join(__dirname, 'public/img', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, "public/img", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -42,13 +39,12 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 const sessionOption = {
   resave: false,
   saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
     secure: false,
   },
 };
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   sessionOption.proxy = true;
 }
 app.use(session(sessionOption));
@@ -56,23 +52,23 @@ app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use('/', pageRouter);
-app.use('/auth', authRouter);
-app.use('/foodsave', foodsaveRouter);
+app.use("/", pageRouter);
+app.use("/auth", authRouter);
+app.use("/foodsave", foodsaveRouter);
 
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-app.listen(app.get('port'), () => {
-  console.log(app.get('port'), '번 포트에서 대기중');
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번 포트에서 대기중");
 });
